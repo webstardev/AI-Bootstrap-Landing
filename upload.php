6c5729ccffc9fe9ffe117822d2fff21fffecae2f
whitebearcoder <whitebearcoder@outlook.com>
2020-11-05 23:14:55 +0200

    add email and file count validate, add multi upload s3

--- upload.php
+++ upload.php
@@ -1,54 +1,64 @@
 <?php
-    
-   putenv('HOME=/var/www');
-   
-   require 'vendor/autoload.php';
-   use Aws\S3\S3Client;
-       
-  ini_set('display_errors', 1);
-   
-  error_reporting(E_ALL);
-        
-  if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
+
+function gen_random_val($length)
+{
+    return substr(str_shuffle(str_repeat($x = '0123456789', ceil($length / strlen($x)))), 1, $length);
+}
+
+putenv('HOME=/var/www');
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //AWS S3 UPLOAD CODE START
-   // Instantiate an Amazon S3 client.
-   $s3 = new S3Client([
-       'version' => 'latest',       
-       'region'  => 'region',
-       'credentials' => [
-				'key'    => "aws_key",
-				'secret' => "aws_secret",
-			]
-   ]);
-   
-   $bucket_name='bucket_name';
-   $folder_name='folder_name';
-   $temp_file_location = $_FILES['upload_file']['tmp_name'];
-   $temp_file_name = $_FILES['upload_file']['name'];
-   
-   $ext = pathinfo($temp_file_name, PATHINFO_EXTENSION);
-   $output = rand(1,9);
-   
-    for($i=0; $i<10; $i++) {
-        $output .= rand(0,9);
+    // Instantiate an Amazon S3 client.
+    $s3 = new S3Client([
+        'version' => 'latest',
+        'region'  => 'region',
+        'credentials' => [
+            'key'    => "aws_key",
+            'secret' => "aws_secret",
+        ]
+    ]);
+
+    $bucket_name = 'bucket_name';
+    $folder_name = 'folder_name';
+
+    $result_array = [];
+
+    //    var_dump($_FILES);
+    $files = $_FILES['upload_files'];
+
+    for ($i = 0; $i < count($files['name']); $i++) {
+        $temp_file_location = $files['tmp_name'][$i];
+        $temp_file_name = $files['name'][$i];
+
+        $ext = pathinfo($temp_file_name, PATHINFO_EXTENSION);
+
+        $output = gen_random_val(10);
+        // $output = strtotime('now');
+        $outputname = $folder_name. '/' . $output . '.' . $ext;
+
+        var_dump($outputname);
+        try {
+            $result_array[] =  $s3->putObject([
+                'Bucket' => $bucket_name,
+                'Key'    => $outputname,
+                'Body'   => fopen($temp_file_location, 'r'),
+                'ContentType'  => $file['type'],       
+            ]);
+
+        } catch (Aws\S3\Exception\S3Exception $e) {
+            echo $e->getMessage();
+            echo "There was an error uploading the file.\n";
+        }
+
     }
-   $outputname=$folder_name.$output.'.'.$ext;
 
-   // Upload a publicly accessible file. The file size and type are determined by the SDK.
-   try {
-      $s3_result=  $s3->putObject([
-           'Bucket' => $bucket_name,
-           'Key'    => $outputname,
-           'Body'   => fopen($temp_file_location, 'r'),
-           'ContentType'  => $_FILES['upload_file']['type'],       
-       ]);
-
-       var_dump($s3_result);
-   
-   } catch (Aws\S3\Exception\S3Exception $e) {
-       echo $e->getMessage();
-       echo "There was an error uploading the file.\n";
-   }
-   //END AWS S3 UPLOAD CODE.
-  }
-   
+}
